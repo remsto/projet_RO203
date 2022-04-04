405faa53e718a95f1417b26da6f13769845e9d7b
Rémi GASTAUD <remi.gastaud@ensta-paritech.fr>
2022-04-04 12:06:54 +0200

    created testInstance and readInputFile fun

--- jeu1/src/io.jl
+++ jeu1/src/io.jl
@@ -4,6 +4,44 @@ using JuMP
 using Plots
 import GR
 
+# Transforme la direction selon le miroir rencontré
+function transfoDirection(dir::Tuple{Int64,Int64}, z_case::Int64)
+    if z_case == 1
+        return (dir[2], dir[1])
+    elseif z_case == 2
+        return (-dir[2], -dir[1])
+    end
+end
+
+# Donne la direction souhaitée pour un côté donné
+function giveDirection(c::Int64)
+    if c == 1
+        return (0, 1)
+    elseif c == 2
+        return (-1, 0)
+    elseif c == 3
+        return (0, -1)
+    elseif c == 4
+        return (1, 0)
+    else
+        println("Problème de valeur dans giveDirection")
+    end
+end
+
+# Donne la case départ pour un côté donnée
+function giveCaseDepart(c::Int64, k::Int64, n::Int64)
+    if c == 1
+        return (k, 1)
+    elseif c == 2
+        return (n, k)
+    elseif c == 3
+        return (k, n)
+    elseif c == 4
+        return (1, k)
+    else
+        println("Problème de valeur dans giveCaseDepart")
+    end
+end
 """
 Read an instance from an input file
 
@@ -18,16 +56,68 @@ function readInputFile(inputFile::String)
     data = readlines(datafile)
     close(datafile)
 
-    # For each line of the input file
-    for line in data
 
-        # TODO
-        println("In file io.jl, in method readInputFile(), TODO: read a line of the input file")
+    n = parse(Int64, split(data[1], ":")[2])
+    V = Array{Int64}(zeros(4, n, n, n))
+    monstre_a_voir = Array{Int64}(undef, 4, n)
+    nb_monstre = Array{Int64}(undef, 3)
+    miroirs = Array{Int64}(undef, n, n)
+
+    # For each line of the input file
+
+    for (i, line) in enumerate(data)
+        if i > 1 && i <= 4
+            nb_monstre[i-1] = parse(Int64, split(line, ":")[2])
+        elseif i == 5
+            split_line = split(line, ",")
+            for (i_element, element) in enumerate(split_line)
+                monstre_a_voir[4, i_element] = parse(Int64, element)
+            end
+        elseif i == 5 + n + 1
+            split_line = split(line, ",")
+            for (i_element, element) in enumerate(split_line)
+                monstre_a_voir[2, i_element] = parse(Int64, element)
+            end
+        elseif i != 1
+            split_line = split(line, ",")
+            monstre_a_voir[1, i-5] = parse(Int64, split_line[1])
+            monstre_a_voir[3, i-5] = parse(Int64, last(split_line))
+            for (i_element, element) in enumerate(split_line)
+                if element == " r"
+                    miroirs[i-5, i_element-1] = 0
+                elseif element == " g"
+                    miroirs[i-5, i_element-1] = 1
+                elseif element == " a"
+                    miroirs[i-5, i_element-1] = 2
+                end
+            end
+
+        end
 
     end
 
+    println(n)
+    # Remplissage des matrices de visibilité
+    for c in 1:4
+        for k in 1:n
+            dir = giveDirection(c)
+            val = 1
+            case = giveCaseDepart(c, k, n)
+            while (case[1] > 0 && case[1] <= n && case[2] > 0 && case[2] <= n)
+                if miroirs[case[1], case[2]] != 0
+                    dir = transfoDirection(dir, miroirs[case[1], case[2]])
+                    val = 2
+                else
+                    V[c, k, case[1], case[2],] = val
+                end
+                case = (case[1] + dir[1], case[2] + dir[2])
+            end
+        end
+    end
+    return n, V, monstre_a_voir, nb_monstre, miroirs
 end
 
+readInputFile("jeu1\\data\\instanceTest.txt")
 
 """
 Create a pdf file which contains a performance diagram associated to the results of the ../res folder
@@ -44,25 +134,25 @@ Prerequisites:
 function performanceDiagram(outputFile::String)
 
     resultFolder = "../res/"
-    
+
     # Maximal number of files in a subfolder
     maxSize = 0
 
     # Number of subfolders
     subfolderCount = 0
 
-    folderName = Array{String, 1}()
+    folderName = Array{String,1}()
 
     # For each file in the result folder
     for file in readdir(resultFolder)
 
         path = resultFolder * file
-        
+
         # If it is a subfolder
         if isdir(path)
-            
+
             folderName = vcat(folderName, file)
-             
+
             subfolderCount += 1
             folderSize = size(readdir(path), 1)
 
@@ -86,16 +176,16 @@ function performanceDiagram(outputFile::String)
 
     # For each subfolder
     for file in readdir(resultFolder)
-            
+
         path = resultFolder * file
-        
+
         if isdir(path)
 
             folderCount += 1
             fileCount = 0
 
             # For each text file in the subfolder
-            for resultFile in filter(x->occursin(".txt", x), readdir(path))
+            for resultFile in filter(x -> occursin(".txt", x), readdir(path))
 
                 fileCount += 1
                 include(path * "/" * resultFile)
@@ -105,11 +195,11 @@ function performanceDiagram(outputFile::String)
 
                     if solveTime > maxSolveTime
                         maxSolveTime = solveTime
-                    end 
-                end 
-            end 
+                    end
+                end
+            end
         end
-    end 
+    end
 
     # Sort each row increasingly
     results = sort(results, dims=2)
@@ -117,10 +207,10 @@ function performanceDiagram(outputFile::String)
     println("Max solve time: ", maxSolveTime)
 
     # For each line to plot
-    for dim in 1: size(results, 1)
+    for dim in 1:size(results, 1)
 
-        x = Array{Float64, 1}()
-        y = Array{Float64, 1}()
+        x = Array{Float64,1}()
+        y = Array{Float64,1}()
 
         # x coordinate of the previous inflexion point
         previousX = 0
@@ -128,7 +218,7 @@ function performanceDiagram(outputFile::String)
 
         append!(x, previousX)
         append!(y, previousY)
-            
+
         # Current position in the line
         currentId = 1
 
@@ -138,7 +228,7 @@ function performanceDiagram(outputFile::String)
             # Number of elements which have the value previousX
             identicalValues = 1
 
-             # While the value is the same
+            # While the value is the same
             while results[dim, currentId] == previousX && currentId <= size(results, 2)
                 currentId += 1
                 identicalValues += 1
@@ -152,10 +242,10 @@ function performanceDiagram(outputFile::String)
                 append!(x, results[dim, currentId])
                 append!(y, currentId - 1)
             end
-            
+
             previousX = results[dim, currentId]
             previousY = currentId - 1
-            
+
         end
 
         append!(x, maxSolveTime)
@@ -165,15 +255,15 @@ function performanceDiagram(outputFile::String)
         if dim == 1
 
             # Draw a new plot
-            plot(x, y, label = folderName[dim], legend = :bottomright, xaxis = "Time (s)", yaxis = "Solved instances",linewidth=3)
+            plot(x, y, label=folderName[dim], legend=:bottomright, xaxis="Time (s)", yaxis="Solved instances", linewidth=3)
 
-        # Otherwise 
+            # Otherwise 
         else
             # Add the new curve to the created plot
-            savefig(plot!(x, y, label = folderName[dim], linewidth=3), outputFile)
-        end 
+            savefig(plot!(x, y, label=folderName[dim], linewidth=3), outputFile)
+        end
     end
-end 
+end
 
 """
 Create a latex file which contains an array with the results of the ../res folder.
@@ -188,10 +278,10 @@ Prerequisites:
 - Each text file contains a variable "solveTime" and a variable "isOptimal"
 """
 function resultsArray(outputFile::String)
-    
+
     resultFolder = "../res/"
     dataFolder = "../data/"
-    
+
     # Maximal number of files in a subfolder
     maxSize = 0
 
@@ -202,7 +292,9 @@ function resultsArray(outputFile::String)
     fout = open(outputFile, "w")
 
     # Print the latex file output
-    println(fout, raw"""\documentclass{article}
+    println(
+        fout,
+        raw"""\documentclass{article}
 
 \usepackage[french]{babel}
 \usepackage [utf8] {inputenc} % utf-8 / latin1 
@@ -221,7 +313,8 @@ function resultsArray(outputFile::String)
 \setlength{\footskip}{27pt} % Bas de page + séparation
 \setlength{\textheight}{668pt} % Hauteur de la zone de texte (25cm)
 
-\begin{document}""")
+\begin{document}"""
+    )
 
     header = raw"""
 \begin{center}
@@ -229,29 +322,29 @@ function resultsArray(outputFile::String)
  \begin{tabular}{l"""
 
     # Name of the subfolder of the result folder (i.e, the resolution methods used)
-    folderName = Array{String, 1}()
+    folderName = Array{String,1}()
 
     # List of all the instances solved by at least one resolution method
-    solvedInstances = Array{String, 1}()
+    solvedInstances = Array{String,1}()
 
     # For each file in the result folder
     for file in readdir(resultFolder)
 
         path = resultFolder * file
-        
+
         # If it is a subfolder
         if isdir(path)
 
             # Add its name to the folder list
             folderName = vcat(folderName, file)
-             
+
             subfolderCount += 1
             folderSize = size(readdir(path), 1)
 
             # Add all its files in the solvedInstances array
-            for file2 in filter(x->occursin(".txt", x), readdir(path))
+            for file2 in filter(x -> occursin(".txt", x), readdir(path))
                 solvedInstances = vcat(solvedInstances, file2)
-            end 
+            end
 
             if maxSize < folderSize
                 maxSize = folderSize
@@ -300,7 +393,7 @@ function resultsArray(outputFile::String)
         if rem(id, maxInstancePerPage) == 0
             println(fout, footer, "\\newpage")
             println(fout, header)
-        end 
+        end
 
         # Replace the potential underscores '_' in file names
         print(fout, replace(solvedInstance, "_" => "\\_"))
@@ -319,9 +412,9 @@ function resultsArray(outputFile::String)
 
                 if isOptimal
                     println(fout, "\$\\times\$")
-                end 
-                
-            # If the instance has not been solved by this method
+                end
+
+                # If the instance has not been solved by this method
             else
                 println(fout, " & - & - ")
             end
@@ -338,5 +431,5 @@ function resultsArray(outputFile::String)
     println(fout, "\\end{document}")
 
     close(fout)
-    
-end 
+
+end
