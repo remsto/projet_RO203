c6582589fd3ab2045df690ee438ce60afea1e4c8
Rémi GASTAUD <remi.gastaud@ensta-paritech.fr>
2022-05-01 16:58:59 +0200

    tests

--- jeu2/src/main.jl
+++ jeu2/src/main.jl
@@ -1,8 +1,8 @@
 
 
-n, I, J, Pa = readInputFile("jeu2/data/instanceTest copy.txt")
+n, I, J, Pa = readInputFile("jeu2/data/instanceTest.txt")
 displayGrid(n, I, J, Pa)
-found, duree, sol = cplexSolve("jeu2/data/instanceTest.txt")
+found, duree, sol = cplexSolve("jeu2/data/instanceTestVnr.txt")
 displaySolution(n, I, J, sol)
 
 
